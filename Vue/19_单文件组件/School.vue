1c62e9c3a343e58f031513853c3e6e54952dc215
psj <729589165@qq.com>
2022-06-27 22:20:47 +0800

    20220627-补充Vue部分

---
+++ Vue/19_单文件组件/School.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+	// 暴露js给外部使用
+	// 写法1:
+	// const school = Vue.extends({...})
+	// export {school}
+	// 写法2：
+	// export default Vue.extends{...}
+	// 写法3：
+    export default {
+        name: "School",
+        data() {
+            return {
+                name: "尚硅谷",
+                address: "北京昌平",
+            };
+        },
+        methods: {
+            showName() {
+                alert(this.name);
+            },
+        },
+    };
+</script>
+
+<style>
+    .demo {
+        background-color: orange;
+    }
+</style>
